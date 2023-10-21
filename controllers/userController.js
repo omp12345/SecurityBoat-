f425f6a4b150bca386b71360ae62a464c30b9482
omp12345 <opsgp1999@gmail.com>
2023-10-21 11:51:16 -0700

    ok

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const User = require('../model/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// Controller for user registration
+exports.registerUser = async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(201).json({ message: 'User already exists' });
+    }
+
+    const { username, email, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ username, email, password: hashedPassword, role });
+    await user.save();
+    res.status(201).json({ message: `${username} registration is successful` });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Controller for user login
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    const token = jwt.sign({ userId: user._id, username: user.username ,role:user.role}, process.env.SECRET_KEY);
+
+    res.status(200).json({ token, msg: 'Login successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to authenticate' });
+  }
+};
